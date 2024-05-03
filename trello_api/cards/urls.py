7b7c7f8ded00a59cbbda087bcd26925a048ca716
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2024-05-03 15:25:54 +0530

    update the Card and Column Models

--- trello_api/cards/urls.py
+++ trello_api/cards/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import AddCardAPIView, EditCardAPIView
+from .views import AddCardAPIView, EditCardAPIView, ReorderCardAPIView
 
 urlpatterns = [
     path('', AddCardAPIView.as_view(), name='add-card'),
     path('<int:pk>/', EditCardAPIView.as_view(), name='edit-card'),
+    path('reorder/', ReorderCardAPIView.as_view(), name='reorder-card'),
 
 ]
--- trello_api/cards/views.py
+++ trello_api/cards/views.py
@@ -39,8 +39,51 @@ class EditCardAPIView(generics.RetrieveUpdateAPIView):
     serializer_class = CardSerializer
 
     def put(self, request, *args, **kwargs):
-        new_column_id = request.data.get('column')
-        last_card_position = Cards.objects.filter(column=new_column_id).values("position")[0]["position"]
-        position = last_card_position + 1 if last_card_position else 0
-        request.data['position'] = position
+        try:
+            card_id = kwargs.get('pk')
+            card = Cards.objects.get(pk=card_id)
+        except Cards.DoesNotExist:
+            return Response({'error': 'Card does not exist'}, status=404)
+        
+        request.data.pop('position', None)
+        
         return self.update(request, *args, **kwargs)
+    
+class ReorderCardAPIView(generics.UpdateAPIView):
+    queryset = Cards.objects.all()
+    serializer_class = CardSerializer
+
+    def put(self, request, *args, **kwargs):
+        try:
+            card_id = kwargs.get('pk')
+            card = Cards.objects.get(pk=card_id)
+        except Cards.DoesNotExist:
+            return Response({'error': 'Card does not exist'}, status=404)
+        
+        new_position = request.data.get('position')
+        column_id = request.data.get('column')
+        
+        try:
+            column = Column.objects.get(id=column_id)
+        except Column.DoesNotExist:
+            return Response({'error': 'Invalid column ID'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if card.column != column:
+            return Response({'error': 'Cannot move card to another column'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if new_position < 0:
+            return Response({'error': 'Invalid position'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        cards = Cards.objects.filter(column=column).order_by('position')
+        for i, c in enumerate(cards):
+            if c.id == card.id:
+                continue
+            if i == new_position:
+                if card.position < new_position:
+                    card.position = i
+                else:
+                    card.position = i - 1
+                card.save()
+                break
+        
+        return Response({'message': 'Card reordered successfully'}, status=status.HTTP_200_OK)
